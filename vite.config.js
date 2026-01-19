832dd1c081f3324bfeac3b38b1ac1aa02d2bdd97
wtaype <wilder.taype16@gmail.com>
2026-01-18 20:25:52 -0500

    Actualizacion Principal v10.10.12

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/wimp3/' : '/',
+  base: '/', 
   build: {
     outDir: 'dist',
     minify: 'esbuild',
